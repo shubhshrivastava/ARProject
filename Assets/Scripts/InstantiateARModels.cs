84689c6ca965be5d6745b82ae1a108cfef48975f
Himanshu05tak <himanshu05.tak@gmail.com>
2020-09-16 20:21:46 +0530

    -Scale and Rotation both updated and worked

--- Assets/Scripts/InstantiateARModels.cs
+++ Assets/Scripts/InstantiateARModels.cs
@@ -6,11 +6,22 @@ using UnityEngine.XR.ARSubsystems;
 [RequireComponent(typeof(ARRaycastManager))]
 public class InstantiateARModels : MonoBehaviour
 {
+    [Header("Intantiate3D-Model")]
     public GameObject instantiatePrefab;
     private GameObject spawnPrefab;
+    public GameObject testTransform;
     private Vector2 touchPosition;
     private ARRaycastManager arRaycastManager;
     private List<ARRaycastHit> arRaycasthitList = new List<ARRaycastHit>();
+
+    [Header("Scaling")]
+    private float initialFingerDistance;
+    private Vector3 initialScale;
+    private Vector3 finalScale;
+
+    [Header("Rotation")]
+    private Touch touchConti;
+    private float getrotatioDir;
 
     private void Awake()
     {
@@ -19,7 +30,7 @@ public class InstantiateARModels : MonoBehaviour
 
     private void Update()
     {
-        if (!TouchInput(touchPosition))
+        if (!TouchInput(out touchPosition, touchConti))
             return;
         if (arRaycastManager.Raycast(touchPosition, arRaycasthitList, TrackableType.PlaneWithinPolygon))
         {
@@ -29,17 +40,78 @@ public class InstantiateARModels : MonoBehaviour
             else
                 spawnPrefab.transform.position = hitPose.position;
         }
-        TouchInput(touchPosition);
     }
 
-    private bool TouchInput(Vector2 _touchPos)
+    private bool TouchInput(out Vector2 _touchPos, Touch _touchcontinue)
     {
-        if (Input.touchCount == 1)
+        if (Input.touchCount > 0)
         {
-            _touchPos = Input.GetTouch(0).position;
-            return true;
+            Touch touch = Input.GetTouch(0);
+            if (Input.touchCount == 1 && touch.phase == TouchPhase.Ended)
+            {
+                Debug.Log("Tap");
+                _touchPos = Input.GetTouch(0).position;
+                return true;
+            }
+            if (Input.touchCount == 2)
+                Scale(touch);
+            if (Input.touchCount == 1)
+                Rotate(touch);
         }
         _touchPos = default;
         return false;
+    }
+
+    private void Rotate(Touch touch)
+    {
+        Debug.Log("Rotation");
+        if (touch.phase == TouchPhase.Moved)
+        {
+            Debug.Log("Moved");
+            getrotatioDir = touch.deltaPosition.y;
+            Debug.Log("calculateRotationDirection" + getrotatioDir);
+            float newgetRotationDirSpeed = getrotatioDir;
+            Debug.Log("newgetRotationDir" + newgetRotationDirSpeed);
+
+#if UNITY_EDITOR
+            testTransform.transform.Rotate(Vector3.up * newgetRotationDirSpeed);
+#endif
+#if !UNITY_EDITOR && UNITY_ANDROID
+                    spawnPrefab.transform.Rotate(Vector3.up * newgetRotationDirSpeed);
+#endif
+        }
+    }
+
+    private void Scale(Touch touch)
+    {
+        if (touch.phase == TouchPhase.Began)
+        {
+            Debug.Log("touchPhase has been started");
+            initialFingerDistance = Vector3.Distance(Input.touches[0].position, Input.touches[1].position);
+            Debug.Log("InitalfigerDistance:" + initialFingerDistance);
+#if !UNITY_EDITOR && UNITY_ANDROID
+                    initialScale = spawnPrefab.transform.localScale;
+#endif
+#if UNITY_EDITOR
+            initialScale = testTransform.transform.localScale;
+#endif
+            Debug.Log("initialScale:" + initialScale);
+        }
+        if (touch.phase == TouchPhase.Moved)
+        {
+            Debug.Log("Moved");
+            float currentFingerDistance = Vector3.Distance(Input.touches[0].position, Input.touches[1].position);
+            Debug.Log("CurrentfingerDistance" + currentFingerDistance);
+            float calculateDistance = currentFingerDistance / initialFingerDistance;
+            Debug.Log("CalculateDistance" + calculateDistance);
+#if !UNITY_EDITOR && UNITY_ANDROID
+                    spawnPrefab.transform.localScale = initialScale * calculateDistance;
+#endif
+#if UNITY_EDITOR
+            testTransform.transform.localScale = initialScale * calculateDistance;
+            Debug.Log("FinalScale" + testTransform.transform.localScale);
+#endif
+        }
+
     }
 }
