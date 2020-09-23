699ec60e5619856f17b2ca029f80cc90d0816290
Himanshu05tak <himanshu05.tak@gmail.com>
2020-09-23 21:19:51 +0530

    -Basic UI Structure Updated

---
+++ Assets/Scripts/UI/BackFunctionality.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+namespace AR_Project.Scripts.AndroidNativeFunctionality
+{
+    public class BackFunctionality : MonoBehaviour
+    {
+        public void Update()
+        {
+#if (!UNITY_EDITOR)
+            if (Application.platform == RuntimePlatform.Android)
+#endif
+            {
+                if (Input.GetKeyDown(KeyCode.Escape))
+                    OnBackEnable();
+            }
+        }
+        public void OnBackEnable()
+        {
+            PopupUI.Instance.backCanvas.gameObject.SetActive(true);
+            
+        }
+
+        public void OnBackDisable()
+        {
+            PopupUI.Instance.backCanvas.gameObject.SetActive(false);
+        }
+    }
+}
+
---
+++ Assets/Scripts/UI/PopupUI.cs
@@ -0,0 +1,59 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Serialization;
+namespace AR_Project.Scripts.AndroidNativeFunctionality
+{
+    public class PopupUI : MonoBehaviour
+    {
+        public static PopupUI Instance { get; private set; }
+        [FormerlySerializedAs("m_BackCanvas")] public GameObject backCanvas;
+        [FormerlySerializedAs("m_LockButton")] public Button lockButton;
+        [FormerlySerializedAs("m_BackFunctionality")] public BackFunctionality backFunctionality;
+        public TextMeshProUGUI text;
+
+        public bool mUnlock;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+                Destroy(gameObject);
+        }
+
+        private void Start()
+        {
+            if (text==null)
+            text = GetComponent<TextMeshProUGUI>();
+            if (string.IsNullOrEmpty(text.text))
+                Unlock(mUnlock);
+        }
+
+        private void Update()
+        {
+            lockButton.onClick.RemoveAllListeners();
+            lockButton.onClick.AddListener(() => { mUnlock = !mUnlock; Unlock(mUnlock); });
+        }
+
+        public void OnYes()
+        {
+            Application.Quit();
+        }
+
+        public void OnNo()
+        {
+            backFunctionality.OnBackDisable();
+        }
+
+        private void Unlock(bool unlock)
+        {
+            text.text = unlock ? "lock" : "unlock";
+
+        }
+    }
+}
+
